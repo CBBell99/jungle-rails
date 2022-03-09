c53070dfe5cd2dda63fb0901b08570b85139cdc5
Chris Bell <chrisbeckertonbell@gmail.com>
2022-03-09 11:40:04 -0800

    fix seed data quantity from 0

--- db/seeds.rb
+++ db/seeds.rb
@@ -128,8 +128,7 @@ cat3.products.create!({
   name:  'Red Bookshelf',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('furniture3.jpg'),
-  quantity: 0,
-  # quantity: 23,
+  quantity: 23,
   price: 2_483.75
 })
 
